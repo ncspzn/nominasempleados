9af47b9d41b97049b6427cfb8e7c971575013170
Nicolas Pinzon <nicolas.pinzong@uqvirtual.edu.co>
2026-04-14 15:39:25 -0500

    Refactor EmpleadoVentas with validation and overrides
    Added validation for totalVentas and porcentajeComision in the constructor. Updated calcularSalarioBruto and mostrarInformacion methods.

--- NominaEmpleados/model/EmpleadoVentas.java
+++ NominaEmpleados/model/EmpleadoVentas.java
@@ -1,26 +1,34 @@
 package NominaEmpleados.model;
 
 public class EmpleadoVentas extends Empleado {
+
     private float totalVentas;
-    private float porcentajeComisions;
-    public EmpleadoVentas(String nombre, String documento, int edad, float salarioBase, CategoriaEmpleado categoria,
-            float descuentoSalud, float descuentopension, float totalVentas, float porcentajeComisions) {
-        super(nombre, documento, edad, salarioBase, categoria, descuentoSalud, descuentopension);
+    private float porcentajeComision;
+
+    public EmpleadoVentas(String nombre, String documento, int edad, float salarioBase,
+                          CategoriaEmpleado categoria, float descuentoSalud, float descuentoPension,
+                          float totalVentas, float porcentajeComision) {
+
+        super(nombre, documento, edad, salarioBase, categoria, descuentoSalud, descuentoPension);
+
+        if (totalVentas < 0) throw new IllegalArgumentException("Ventas inválidas");
+        if (porcentajeComision < 0 || porcentajeComision > 100)
+            throw new IllegalArgumentException("Comisión inválida");
+
         this.totalVentas = totalVentas;
-        this.porcentajeComisions = porcentajeComisions;
+        this.porcentajeComision = porcentajeComision;
     }
-    public float getTotalVentas() {
-        return totalVentas;
+
+    @Override
+    public float calcularSalarioBruto() {
+        float comision = (totalVentas * porcentajeComision) / 100;
+        return salarioBase + calcularBonificacionCategoria() + comision;
     }
-    public void setTotalVentas(float totalVentas) {
-        this.totalVentas = totalVentas;
+
+    @Override
+    public String mostrarInformacion() {
+        return super.mostrarInformacion() +
+               "\nTipo: Ventas" +
+               "\nSalario Neto: " + calcularSalarioNeto();
     }
-    public float getPorcentajeComisions() {
-        return porcentajeComisions;
-    }
-    public void setPorcentajeComisions(float porcentajeComisions) {
-        this.porcentajeComisions = porcentajeComisions;
-    }
-    
-    
 }
