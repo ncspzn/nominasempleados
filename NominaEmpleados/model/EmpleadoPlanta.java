c131f3622be083f207876db2cc91bc876a182473
Nicolas Pinzon <nicolas.pinzong@uqvirtual.edu.co>
2026-04-14 15:39:50 -0500

    Refactor EmpleadoPlanta constructor with validation
    Added validation for horasExtra, valorHoraExtra, and auxilioTransporte in the constructor.

--- NominaEmpleados/model/EmpleadoPlanta.java
+++ NominaEmpleados/model/EmpleadoPlanta.java
@@ -1,43 +1,39 @@
 package NominaEmpleados.model;
 
-public class EmpleadoPlanta extends Empleado{
-private String cargo;
-private int horasExtra;
-private float valorHoraExtra;
-private float auxilioTransporte;
-public EmpleadoPlanta(String nombre, String documento, int edad, float salarioBase, CategoriaEmpleado categoria,
-        float descuentoSalud, float descuentopension, String cargo, int horasExtra, float valorHoraExtra,
-        float auxilioTransporte) {
-    super(nombre, documento, edad, salarioBase, categoria, descuentoSalud, descuentopension);
-    this.cargo = cargo;
-    this.horasExtra = horasExtra;
-    this.valorHoraExtra = valorHoraExtra;
-    this.auxilioTransporte = auxilioTransporte;
-}
-public String getCargo() {
-    return cargo;
-}
-public void setCargo(String cargo) {
-    this.cargo = cargo;
-}
-public int getHorasExtra() {
-    return horasExtra;
-}
-public void setHorasExtra(int horasExtra) {
-    this.horasExtra = horasExtra;
-}
-public float getValorHoraExtra() {
-    return valorHoraExtra;
-}
-public void setValorHoraExtra(float valorHoraExtra) {
-    this.valorHoraExtra = valorHoraExtra;
-}
-public float getAuxilioTransporte() {
-    return auxilioTransporte;
-}
-public void setAuxilioTransporte(float auxilioTransporte) {
-    this.auxilioTransporte = auxilioTransporte;
-}
+public class EmpleadoPlanta extends Empleado {
 
-    
+    private String cargo;
+    private int horasExtra;
+    private float valorHoraExtra;
+    private float auxilioTransporte;
+
+    public EmpleadoPlanta(String nombre, String documento, int edad, float salarioBase,
+                          CategoriaEmpleado categoria, float descuentoSalud, float descuentoPension,
+                          String cargo, int horasExtra, float valorHoraExtra, float auxilioTransporte) {
+
+        super(nombre, documento, edad, salarioBase, categoria, descuentoSalud, descuentoPension);
+
+        if (horasExtra < 0) throw new IllegalArgumentException("Horas inválidas");
+        if (valorHoraExtra < 0) throw new IllegalArgumentException("Valor hora inválido");
+        if (auxilioTransporte < 0) throw new IllegalArgumentException("Auxilio inválido");
+
+        this.cargo = cargo;
+        this.horasExtra = horasExtra;
+        this.valorHoraExtra = valorHoraExtra;
+        this.auxilioTransporte = auxilioTransporte;
+    }
+
+    @Override
+    public float calcularSalarioBruto() {
+        float pagoHoras = horasExtra * valorHoraExtra;
+        return salarioBase + calcularBonificacionCategoria() + pagoHoras + auxilioTransporte;
+    }
+
+    @Override
+    public String mostrarInformacion() {
+        return super.mostrarInformacion() +
+               "\nTipo: Planta" +
+               "\nCargo: " + cargo +
+               "\nSalario Neto: " + calcularSalarioNeto();
+    }
 }
